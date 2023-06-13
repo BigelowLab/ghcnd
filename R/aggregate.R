4b2ac6589904c1072492e5e337c94c1231d3d894
Ben Tupper <btupper@bigelow.org>
2023-06-13 14:07:22 -0400

    add aggregate_annual and complete_intervals

--- R/aggregate.R
+++ R/aggregate.R
@@ -1,3 +1,50 @@
+#' Clip a table so that only complete intervals are present (for subsequent aggregation)
+#'
+#' @param x tibble with 'STATION' and 'DATE' variable
+#' @param by character, one of "year" (default) or "month"
+#' @param min_count numeric defaults to 364, but adjust to 28 for month
+#' @return tibble clipped to include only complete intervals
+complete_intervals = function(x, by = c("year", "month")[1], min_count = 364){
+  fmt = switch(tolower(by[1]),
+               "year" = "%Y-01-01",
+               "month" = "%Y-%m-01")
+  dplyr::mutate(x, interval_ = format(.data$DATE, fmt)) |>
+  dplyr::group_by(STATION, interval_) |>
+  dplyr::group_map(
+    function(tbl, key){
+      if (nrow(tbl) < min_count){
+        return(NULL)
+      } else {
+        return(tbl)
+      }
+    }, .keep = TRUE) |>
+  dplyr::bind_rows() |>
+  dplyr::select(-dplyr::all_of("interval_"))
+}
+
+
+#' Aggregate into annual means (even precip)
+#'
+#' @export
+#' @param x tibble of GHCNd station data
+#' @return monthly summary (by station) tibble
+aggregate_annual = function(x){
+  lut = station_lut(x)
+  nm = lut$NAME |>
+    rlang::set_names(lut$STATION)
+  
+  complete_intervals(x, by = "year", min_count = 364) |>
+    dplyr::mutate(date = as.Date(format(DATE, "%Y-01-01")), .after = STATION) |>
+    dplyr::select(-dplyr::all_of(c("DATE", "ELEVATION"))) |>
+    dplyr::rename(DATE = date) |>
+    dplyr::group_by(STATION, DATE) |>
+    dplyr::summarize(across(dplyr::where(is.numeric), \(x) mean(x, na.rm = TRUE))) |>
+    dplyr::mutate(NAME = nm[STATION], .after = STATION) |>
+    dplyr::mutate(YEAR = as.numeric(format(DATE, "%Y")), .after = DATE)
+}
+
+
+
 #' Aggregate into monthly means (even precip)
 #'
 #' @export
@@ -7,13 +54,15 @@ aggregate_monthly = function(x){
   lut = station_lut(x)
   nm = lut$NAME |>
     rlang::set_names(lut$STATION)
-  dplyr::mutate(x, date = as.Date(format(DATE, "%Y-%m-01")), .after = STATION) |>
-    dplyr::select(-dplyr::all_of(c("DATE", "ELEVATION"))) |>
-    dplyr::rename(DATE = date) |>
-    dplyr::group_by(STATION, DATE) |>
-    dplyr::summarize(across(dplyr::where(is.numeric), \(x) mean(x, na.rm = TRUE))) |>
-    dplyr::mutate(NAME = nm[STATION], .after = STATION) |>
-    dplyr::mutate(MONTH = factor(format(DATE, "%b"), levels = month.abb), .after = DATE)
+  
+  complete_intervals(x, by = "month", min_count = 28) |>
+  dplyr::mutate(date = as.Date(format(DATE, "%Y-%m-01")), .after = STATION) |>
+  dplyr::select(-dplyr::all_of(c("DATE", "ELEVATION"))) |>
+  dplyr::rename(DATE = date) |>
+  dplyr::group_by(STATION, DATE) |>
+  dplyr::summarize(across(dplyr::where(is.numeric), \(x) mean(x, na.rm = TRUE))) |>
+  dplyr::mutate(NAME = nm[STATION], .after = STATION) |>
+  dplyr::mutate(MONTH = factor(format(DATE, "%b"), levels = month.abb), .after = DATE)
 }
 
 #' Compute anomalies for numeric columns in a grouped table
